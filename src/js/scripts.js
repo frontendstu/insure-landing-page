d9c76a18511ad7811a5a022a36f89b372d00e1a8
Stu Cowley <stucowley@outlook.com>
2024-07-27 19:48:32 +0930

    refactor(scripts.js): update header menu behaviour. By removing tapping outside to close menu, and make menu fill the viewport by adding the class .header__no-scroll

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,6 +7,7 @@ console.log('Hello, World!');
 document.addEventListener('DOMContentLoaded', function () {
     const headerMenuToggle = document.querySelector('.header__toggle');
     const headerMenuNav = document.querySelector('#header-nav');
+    const body = document.body;
 
     headerMenuToggle.addEventListener('click', function () {
         const isMenuVisible = headerMenuNav.hasAttribute('data-visible');
@@ -14,23 +15,11 @@ document.addEventListener('DOMContentLoaded', function () {
         if (isMenuVisible) {
             headerMenuNav.removeAttribute('data-visible');
             headerMenuToggle.setAttribute('aria-expanded', 'false');
+            body.classList.remove('header__no-scroll');
         } else {
             headerMenuNav.setAttribute('data-visible', '');
             headerMenuToggle.setAttribute('aria-expanded', 'true');
-
-            // Optional: Close menu on click outside
-            document.addEventListener('click', closeMenuOnClickOutside);
+            body.classList.add('header__no-scroll');
         }
     });
-
-    function closeMenuOnClickOutside(event) {
-        if (
-            !headerMenuNav.contains(event.target) &&
-            !headerMenuToggle.contains(event.target)
-        ) {
-            headerMenuNav.removeAttribute('data-visible');
-            headerMenuToggle.setAttribute('aria-expanded', 'false');
-            document.removeEventListener('click', closeMenuOnClickOutside);
-        }
-    }
 });
