954e3cb6657e6ca8a94eae99b10952aed6fd598c
Stu Cowley <stucowley@outlook.com>
2024-08-22 13:02:27 +0930

    feat(scripts.js): add function to dynamically change the header menu button theme depending on viewport size

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -7,7 +7,26 @@ console.log('Hello, World!');
 document.addEventListener('DOMContentLoaded', function () {
     const headerMenuToggle = document.querySelector('.header__toggle');
     const headerMenuNav = document.querySelector('#header-nav');
+    const btn = document.querySelector('.header__menu .btn');
 
+    // Function to update the button style based on viewport size
+    function updateButtonStyle() {
+        if (window.matchMedia('(max-width: 47.98em)').matches) {
+            // Apply 'on-dark' style for mobile view
+            btn.setAttribute('data-btn-style', 'on-dark');
+        } else {
+            // Apply 'on-light' style for larger viewports
+            btn.setAttribute('data-btn-style', 'on-light');
+        }
+    }
+
+    // Initial update of button style
+    updateButtonStyle();
+
+    // Update button style on viewport resize
+    window.addEventListener('resize', updateButtonStyle);
+
+    // Toggle menu visibility on button click
     headerMenuToggle.addEventListener('click', function () {
         const isMenuVisible = headerMenuNav.hasAttribute('data-visible');
 
@@ -23,6 +42,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
+    // Function to close the menu when clicking outside
     function closeMenuOnClickOutside(event) {
         if (
             !headerMenuNav.contains(event.target) &&
